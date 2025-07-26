604f43b5e79a8ed558e35a93bbc844ec6f37cef7
IgorMitrofanov <i.v.mitrofanov@ya.ru>
2025-07-26 16:38:06 +0300

    update constants

--- handlers/utils.py
+++ handlers/utils.py
@@ -1,7 +1,7 @@
 import random
 
 def calculate_typing_delay(text: str) -> float:
-    base_speed = 10  # знаков в секунду
+    base_speed = 20  # знаков в секунду
     
     # Базовое время набора
     base_time = len(text) / base_speed
@@ -18,6 +18,6 @@ def calculate_typing_delay(text: str) -> float:
     
     # Итоговый расчет
     total_delay = (base_time + sentence_pauses + comma_pauses) * speed_variation
-    total_delay = max(1.5, min(8, total_delay)) 
+    total_delay = max(1.0, min(6, total_delay)) 
     
     return round(total_delay, 1)
